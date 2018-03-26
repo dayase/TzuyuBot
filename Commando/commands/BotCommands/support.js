69a2208cd996d4cceffc343ae83407f4cb1a1cde
DubuTofu <37800668+DubuTofu@users.noreply.github.com>
2018-03-26 14:37:46 +0200

    changed support.js

--- Commando/commands/BotCommands/support.js
+++ Commando/commands/BotCommands/support.js
@@ -1,1 +1,22 @@
+const { Command } = require('discord.js-commando')
+const { RichEmbed } = require('discord.js')
 
+module.exports = class SupportCommand extends Command {
+    constructor(client){
+        super(client, {
+            name: 'support',
+			group: 'bc',
+            memberName: 'support',
+			description: 'Invite to the Bot Support Server'
+        })
+    }
+    async run(msg) {
+		if (!msg.guild) return msg.reply('This is not a guild!');
+		let embed = new RichEmbed()
+            .setAuthor('Tzuyu')
+            .setDescription('Support Server Invite')
+			.setColor("6a37bd")
+			.addField("💻 Support Server:", `[Click Here](https://discord.gg/WrVzkH)`)
+		return msg.channel.send(embed)
+	}
+}
