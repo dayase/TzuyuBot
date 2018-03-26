02d84e368039c53d9d20a9024bd02342a678aae2
DubuTofu <37800668+DubuTofu@users.noreply.github.com>
2018-03-26 14:45:54 +0200

    Changed boring look of invite.js to super embed

--- Commando/commands/BotCommands/invite.js
+++ Commando/commands/BotCommands/invite.js
@@ -1,21 +1,22 @@
-const { Command } = require('discord.js-commando');
+const { Command } = require('discord.js-commando')
+const { RichEmbed } = require('discord.js')
 
-module.exports = class ReplyCommand extends Command {
-    constructor(client) {
+module.exports = class SupportCommand extends Command {
+    constructor(client){
         super(client, {
             name: 'invite',
             aliases: ['inv'],
-            group: 'bc',
-            memberName: 'invite',
-            description: 'The Invite of the Bot',
-            examples: ['invite'],
-            throttling: {
-                usages: 1,
-                duration: 10
-            }
+			group: 'bc',
+            memberName: 'ivite',
+			description: 'Invite to the Bot to your Server'
         })
     }
-run(msg) {
-    return msg.say('```https://discordapp.com/api/oauth2/authorize?client_id=425757845858942976&permissions=1580592215&scope=bot```');
-   }
+    async run(msg) {
+		let embed = new RichEmbed()
+            .setAuthor('Tzuyu')
+            .setDescription('Bot Invite Link')
+			.setColor("6a37bd")
+			.addField("🔗 Invite Link:", `[Click Here](https://discordapp.com/api/oauth2/authorize?client_id=425757845858942976&permissions=1580592215&scope=bot)`)
+		return msg.channel.send(embed)
+	}
 }
